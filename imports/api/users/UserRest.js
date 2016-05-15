104520b8c6097154a3dcd1b066bac4ef0a5c6944
Fatih Dogru <ftdogru@yahoo.com>
2016-05-15 13:32:11 +0300

    Set custom user auth via rest endpoint

--- imports/api/users/UserRest.js
+++ imports/api/users/UserRest.js
@@ -3,3 +3,39 @@ import { Meteor } from 'meteor/meteor';
 
 
 Api.addCollection(Meteor.users);
+
+Api.addRoute('yellfi/login',{autRequired:false},{
+	post : function () {
+	console.log('email : ' +this.bodyParams.email)
+
+	if (this.bodyParams.email) {
+		key = 'email'
+		value = this.bodyParams.email
+	} else {
+		key = 'username'
+		value = this.bodyParams.username
+	}
+		//http call for login
+	
+   response= HTTP.call( 'POST', 'http://192.168.1.4:4000/api/login', {
+				  data: {
+				   [key] : value,
+				   password :  this.bodyParams.password
+				  }
+				});
+
+	if (response.data.data.userId) {
+		 user = Meteor.users.findOne({"_id" : response.data.data.userId }); 
+		    console.log(user._id + ' ' + user.username  )
+		    delete user.services
+		    user.authToken = response.data.data.authToken
+		    res = user
+		} else {
+			res = "wrong information"
+		}
+//http call end
+
+		
+		return res
+	}
+})
